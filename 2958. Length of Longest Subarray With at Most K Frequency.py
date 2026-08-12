257dcc945f0edf56cd846f66439861084aaaf20c
ELFANTINHO <140125432+karimtz999@users.noreply.github.com>
2026-08-12 23:45:13 +0100

    Update 2958. Length of Longest Subarray With at Most K Frequency.py

--- 2958. Length of Longest Subarray With at Most K Frequency.py
+++ 2958. Length of Longest Subarray With at Most K Frequency.py
@@ -1,6 +1,7 @@
 from typing import List
 from collections import defaultdict
 
+
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
         # Dictionary to track frequency of each element in current window
