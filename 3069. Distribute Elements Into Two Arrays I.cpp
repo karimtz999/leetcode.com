91dfd5dc69ef63e42bfc9695de37a0d0d66b8caf
ELFANTINHO <140125432+karimtz999@users.noreply.github.com>
2026-08-20 23:49:47 +0100

    Update 3069. Distribute Elements Into Two Arrays I.cpp

--- 3069. Distribute Elements Into Two Arrays I.cpp
+++ 3069. Distribute Elements Into Two Arrays I.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> resultArray(vector<int>& nums) {
         int n = nums.size();
         
-      
         // Initialize first array with the first element
         vector<int> firstArray = {nums[0]};
 
