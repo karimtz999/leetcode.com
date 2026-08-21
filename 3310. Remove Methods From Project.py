33be283da568e42b58b8ac291dcd64502a7e5b50
ELFANTINHO <140125432+karimtz999@users.noreply.github.com>
2026-08-21 16:10:29 +0100

    Update 3310. Remove Methods From Project.py

--- 3310. Remove Methods From Project.py
+++ 3310. Remove Methods From Project.py
@@ -9,6 +9,7 @@ class Solution:
             undirected_graph[callee].append(caller)
             is_suspicious = [False] * n
             
+            
 
             
         def mark_suspicious_methods(method_id: int) -> None:
