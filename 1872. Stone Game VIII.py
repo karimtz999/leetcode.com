33f31a4ade0b1194e00b7e4cc7d009dd733f4495
ELFANTINHO <140125432+karimtz999@users.noreply.github.com>
2026-08-24 23:52:55 +0100

    Update 1872. Stone Game VIII.py

--- 1872. Stone Game VIII.py
+++ 1872. Stone Game VIII.py
@@ -2,7 +2,6 @@ from typing import List
 from functools import cache
 from itertools import accumulate
 
-
 class Solution:
     def stoneGameVIII(self, stones: List[int]) -> int:
         prefix_sums = list(accumulate(stones))
